92664e013b7c31708e42237f3483e26a84fa6b34
Chase Allen <callen@passageways.com>
2019-04-02 06:00:23 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div class="app-background"></div>
+    <header class="app-header">LoCoMan Music</header>
+    <div class="app-nav">
+      <router-link to="/">Home</router-link>
+    </div>
+    <!-- -->
+    <router-view/>
+    <!-- -->
+    <footer class="app-footer">1986 - {{ new Date().getFullYear() }} © LoCoMan</footer>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  position: relative;
+  min-height: 100vh;
+  // black background-color set in index.html body {}
+  color: white;
+
+  .app-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background: url("https://steamuserimages-a.akamaihd.net/ugc/921427477594349929/06096042B4C75B57A239B34187E05129DCDAC5E4/")
+      no-repeat center / cover;
+    filter: blur(2px);
+    opacity: 0.2;
+  }
+
+  .app-header {
+    margin-bottom: 1rem;
+    padding: 1rem 0;
+    background-color: black;
+    font-size: 40px;
+    text-shadow: 0 1px 0 #e43526, 0 0 16px #e43526;
+  }
+
+  .app-nav {
+    // router-link
+    a {
+      display: inline-block;
+      margin-bottom: 1rem;
+      color: white;
+    }
+  }
+
+  .app-footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    padding: 0.66rem 0 0.5rem;
+    width: 100%;
+    background-color: black;
+    font-size: 0.5rem;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="view-home">
+    <iframe
+      class="reverb-nation-player"
+      scrolling="no"
+      frameborder="no"
+      src="https://www.reverbnation.com/widget_code/html_widget/artist_3865698?widget_id=55&pwc[included_songs]=1&context_type=page_object&spoid=artist_3865698"
+    ></iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.view-home {
+  .reverb-nation-player {
+  display: block;
+  margin: 0 auto 2rem;
+  width: 300px;
+  height: 390px
+}
+}
+</style>
+
+
+<script>
+export default {
+  name: "home"
+};
+</script>
